8d28ea71dcae4cf284aff3eadb66410561c7a9f4
andresvela <andresvela@gmail.com>
2013-03-08 11:50:11 +0100

    With dataDuplication!

--- CFCMailCleaner/FlatTransofmer.cs
+++ CFCMailCleaner/FlatTransofmer.cs
@@ -24,10 +24,10 @@ namespace CFCMailCleaner
         private List<string[]> documentLines = new List<string[]>();
         private CommandLineRader commandReader;
         private MemoryStream sw;
+        private XmlWriter writer;
 
 
         private string xmlmStringValue;
-
         public string XmlmStringValue
         {
             get { return xmlmStringValue; }            
@@ -53,6 +53,113 @@ namespace CFCMailCleaner
             return null;
         }
 
+        private bool createModeleNode(string[] templateData, int index) {                                    
+                this.writer.WriteElementString("MODELE", templateData[index]);
+                return true;     
+        }
+
+        private bool createEmailNode(string[] templateData)
+        {
+            int i = 0;
+            writer.WriteStartElement("EMAILDATA");
+            foreach (string emailD in emailData)
+            {
+                if (i != 0 && i != 1 && i != emailData.Length - 1)
+                    writer.WriteElementString("EMAIL_" + (i - 1), emailData[i]);
+                i++;
+            }
+            writer.WriteEndElement();
+            return true;
+        }
+
+        private bool createCDATANode(int lineHeaderIni, int lineHeaderEn)
+        {
+            writer.WriteStartElement("FLATDATA");
+            writer.WriteCData("\r\n" + string.Join("\r\n", System.IO.File.ReadLines(this.commandReader.FileName, Encoding.GetEncoding("iso-8859-1")).Skip(lineHeaderIni - 1).Take(lineHeaderEn - lineHeaderIni + 1).ToArray()) + "\r\n");
+            writer.WriteEndElement();
+            return true;
+        }
+
+        private string getHeader (string infoLine){
+            string finalHeader=null;
+            try
+            {
+                finalHeader = infoLine.Substring(1, infoLine.IndexOf("#", 2) - 1);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("getHeader ERROR:" + ex.Message);
+            }
+            return finalHeader;
+        }
+
+        private bool getXMLBody() {
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineIndex++;
+                //read header line      
+                //add try catch
+                headerLine = getHeader(line);
+
+                //documentSeparator traitement
+                if (headerLine == this.commandReader.DocumentSeparator)
+                {
+                    //detect header document
+                    if (lineHeaderIN == false)
+                    {
+
+                        lineHeaderInit = lineIndex;
+                        lineHeaderIN = true;
+                        writer.WriteStartElement("BDOCEDITDATA");
+                    }
+
+                    //detect end document                                
+                    else
+                    {
+
+                        lineHeaderEnd = lineIndex - 1;
+                        createCDATANode(lineHeaderInit, lineHeaderEnd);
+                        writer.WriteEndElement();
+
+                        //write ducplicate if needed
+                        if (templateData.Length > 4)
+                        {
+                            writer.WriteStartElement("BDOCEDITDATA");
+                            createModeleNode(templateData, 3);
+                            createEmailNode(emailData);
+                            createCDATANode(lineHeaderInit, lineHeaderEnd);
+                            writer.WriteEndElement();
+                        }
+                        //new Header
+                        lineHeaderInit = lineIndex;
+                        writer.WriteStartElement("BDOCEDITDATA");
+                    }
+
+                }
+
+                //template data Treatment
+                if (headerLine == this.commandReader.TemplateStructSeparator)
+                {
+                    templateData = line.Split(delimiterChars);
+                    createModeleNode(templateData, 2);
+                }
+
+                //email data Treatment
+                if (headerLine == this.commandReader.EmailDataSeparator)
+                {
+                    emailData = line.Split(delimiterChars);
+                    createEmailNode(emailData);
+                }
+            }
+
+            //detect end document       
+            lineHeaderEnd = lineIndex--;
+            createCDATANode(lineHeaderInit, lineHeaderEnd);
+            return true;
+        
+        }
+
         public bool doTransformation()
         {
             using (sw = new MemoryStream())
@@ -63,80 +170,16 @@ namespace CFCMailCleaner
                 xmlWriterSettings.Indent = true;
 
 
-                using (var writer = System.Xml.XmlWriter.Create(sw, xmlWriterSettings))
-                {
-                    // Build Xml with xw.                    
+                using (writer = System.Xml.XmlWriter.Create(sw, xmlWriterSettings))
+                {                                       
                     writer.WriteStartDocument();
                     writer.WriteStartElement("ROOT");
 
-
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        lineIndex++;
-                        //read header line      
-                        //add try catch
-                        headerLine = line.Substring(1, line.IndexOf("#", 2) - 1);
-
-                        //documentSeparator traitement
-                        if (headerLine == this.commandReader.DocumentSeparator)
-                        {
-                            //detect header document
-                            if (lineHeaderIN == false)
-                            {
-                               
-                                lineHeaderInit = lineIndex;
-                                lineHeaderIN = true;
-                                writer.WriteStartElement("BDOCEDITDATA");
-                            }
-
-                            //detect end document
-                                
-                            else
-                            {
-                                //lineHeaderIN = false;
-                                lineHeaderEnd = lineIndex-1;
-
-                                writer.WriteCData("\r\n" + string.Join("\r\n", System.IO.File.ReadLines(this.commandReader.FileName, Encoding.GetEncoding("iso-8859-1")).Skip(lineHeaderInit - 1).Take(lineHeaderEnd - lineHeaderInit+1).ToArray()) + "\r\n");
-                                writer.WriteEndElement();
-                                //new Header
-                                lineHeaderInit = lineIndex;
-                                writer.WriteStartElement("BDOCEDITDATA");
-                            }
-                                 
-                        }
-
-                        //template data Treatment
-                        if (headerLine == this.commandReader.TemplateStructSeparator)
-                        {
-                            templateData = line.Split(delimiterChars);
-                            writer.WriteElementString("MODELE", templateData[2]);
-                        }
-
-                        //email data Treatment
-                        if (headerLine == this.commandReader.EmailDataSeparator)
-                        {
-                            emailData = line.Split(delimiterChars);
-                            writer.WriteElementString("EMAIL", emailData[2]);
-                        }
-
-                        if (headerLine == this.commandReader.DuplicateStructSeparator)
-                        {
-                            //duplicate data Treatment
-                        }
-                        //this.xmlmStringValue = Encoding.UTF8.GetString(sw.ToArray());
-                    }
-
-                    //detect end document       
-                    lineHeaderEnd = lineIndex--;
-                    //print info between lineHeaderInit and lineHeaderEnd
-                   // writer.WriteStartElement("FLATDATA");
-                    writer.WriteCData("\r\n" + string.Join("\r\n", System.IO.File.ReadLines(this.commandReader.FileName, Encoding.GetEncoding("iso-8859-1")).Skip(lineHeaderInit).Take(lineHeaderEnd - 1).ToArray()) + "\r\n");
-                   // writer.WriteEndElement();
+                    getXMLBody();
+                    
                     writer.WriteEndElement();
-                    writer.WriteEndDocument();
-                     
+                    writer.WriteEndDocument();                     
                     this.xmlmStringValue = Encoding.UTF8.GetString(sw.ToArray());
-
                 }
             }
             return true;
